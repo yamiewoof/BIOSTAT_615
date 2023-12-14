a2ab348bed9647b6d67356670512ef4004033f0e
Chi-Hsiang Yi <carlyi@umich.edu>
2023-12-14 15:27:58 -0500

    added guide in package installation

--- std_vs_aug_for_graph_runtime_comparison.R
+++ std_vs_aug_for_graph_runtime_comparison.R
@@ -1,7 +1,10 @@
 ###
-# Testing with data generated from ADMM package
-# Standard ADMM vs augADMM for graph
+# Script Purpose: Evaluate and compare the runtime of standard ADMM and augmented ADMM 
+# specifically for graph-structured problems. The script generates a plot showing 
+# runtime differences between the two methods across varying problem sizes.
 ###
+ 
+setwd("augmentedADMM")
 
 library(microbenchmark)
 library(ggplot2)
@@ -11,26 +14,27 @@ source("gen_data_v2.R")
 
 set.seed(123)
 
-# Define different problem sizes
-problem_sizes <- seq(500, 1000, by = 50)
+# Define a range of problem sizes for benchmarking
+problem_sizes <- seq(100, 200, by = 25)
 
 # Data frame to store the results
 benchmark_results <- data.frame(size = integer(), method = character(), time = numeric())
 
 for (dim in problem_sizes) {
-  data_params <- list(num.groups = 5, num.vars.per.group=100,
-                      n = dim, num.active.groups=2, cor=0.7, err.var=0.5)
+  data_params <- list(num.groups = dim, num.vars.per.group=11,
+                      n = 200, num.active.groups=4, cor=0.7, err.var=0.1)
   data <- do.call(gen_data, data_params)
 
+  # Extract matrices and vectors from generated data
   X <- data$X
   Y <- data$Y
-  D <- data$A
-  C <- data$C
-  M <- data$M
+  D <- data$A  # Incidence matrix for the graph
+  C <- data$C  # Constraint matrix
+  M <- data$M  # Matrix for augmented ADMM
 
 
   # Set regularization lambda value
-  lambda1 <- 5
+  lambda1 <- 0.5
   lambda2 <- 0.1
 
   # Benchmark standard ADMM
@@ -56,13 +60,13 @@ for (dim in problem_sizes) {
 benchmark_results$time <- benchmark_results$time / 1e9
 
 # Plot the results
-ggplot(benchmark_results, aes(x = size, y = time, color = method)) +
+ggplot(benchmark_results, aes(x = size * 11, y = time, color = method)) +
   geom_line() +
   geom_point() +
   labs(title = "Run Time Comparison between Standard ADMM and Augmented ADMM for graph",
-       x = "Problem Size (n)", y = "Time (seconds)") +
+       x = "Problem Size (p)", y = "Time (seconds)") +
   theme_minimal() +
   scale_y_continuous(labels = scales::comma) # To format the y axis with commas
 
 # Save the plot to a file
-ggsave("std_vs_aug_admm_for_graph_runtime_comparison.pdf", width = 10, height = 6)
+ggsave("output/std_vs_aug_admm_for_graph_runtime_comparison.png", device = "png", width = 10, height = 6, dpi = 300)
